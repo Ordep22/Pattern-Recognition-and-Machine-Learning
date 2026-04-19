6a5598ff4d73090adc6971b9644fa333d1b7399b
Pedro Pereira <pedropereira@Pedros-MacBook-Pro.local>
2026-04-19 18:50:34 -0300

    Assignment 02: Titanic passengers survived prediction - cleaning data and train basic model - Done with Confusion Matrix

--- Assignment_two/main.py
+++ Assignment_two/main.py
@@ -10,9 +10,17 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
 # Load the Titanic dataset (Training and Testing sets)
-df_gender_submission  = pd.read_csv(r"/Users/pedropereira/Documents/Work/Pattern-Recognition-and-Machine-Learning/Assignment_two/titanic/gender_submission.csv")
-df_test = pd.read_csv(r"/Users/pedropereira/Documents/Work/Pattern-Recognition-and-Machine-Learning/Assignment_two/titanic/test.csv")
-df_train = pd.read_csv(r"/Users/pedropereira/Documents/Work/Pattern-Recognition-and-Machine-Learning/Assignment_two/titanic/train.csv")
+df_gender_submission  = pd.read_csv(r"/Users/pedropereira/Documents/" \
+"Work/Pattern-Recognition-and-Machine-Learning/Assignment_two/titanic/gender_submission.csv")
+df_test = pd.read_csv(r"/Users/pedropereira/Documents/" \
+"Work/Pattern-Recognition-and-Machine-Learning/Assignment_two/titanic/test.csv")
+df_train = pd.read_csv(r"/Users/pedropereira/Documents/" \
+"Work/Pattern-Recognition-and-Machine-Learning/Assignment_two/titanic/train.csv")
+
+df_test['Title'] = df_test['Name'].str.extract(' ([A-Za-z]+)\\.', expand = False)
+print(df_test['Title'])
+df_train['Title'] = df_train['Name'].str.extract(' ([A-Za-z]+)\\.', expand = False)
+print(df_train.head())
 
 
 # Feature Selection: Dropping columns that do not 
@@ -46,6 +54,32 @@ df_train['Embarked'] = df_train['Embarked'].fillna('S')
 df_train['Embarked'] = df_train['Embarked'].map({'S':0.0,'C':1.0,'Q':2.0})
 
 
+df_test['FamilySize'] = df_test['SibSp'] + df_test['Parch'] + 1
+median_family_size_test  = df_test['FamilySize'].mean()
+df_test['FamilySize'] = df_test['FamilySize'].fillna(median_family_size_test)
+
+
+df_train['FamilySize'] = df_train['SibSp'] + df_train['Parch'] + 1
+median_family_size_train  = df_train['FamilySize'].mean()
+df_train['FamilySize'] = df_train['FamilySize'].fillna(median_family_size_train)
+
+
+title_mapping = {
+    "Mr": 0, "Miss": 1, "Mrs": 2, "Master": 3, 
+    "Col": 4, "Dr": 4, "Rev": 4, "Major": 4, "Mlle": 1, "Ms": 1, "Mme": 2, 
+    "Lady": 4, "Countess": 4, "Sir": 4, "Don": 4, "Dona": 4, "Jonkheer": 4, "Capt": 4
+}
+
+# Apply the mapping
+df_train['Title'] = df_train['Title'].map(title_mapping)
+df_test['Title'] = df_test['Title'].map(title_mapping)
+
+
+mode_title = df_train['Title'].mode()[0]
+df_train['Title'] = df_train['Title'].fillna(mode_title)
+df_test['Title'] = df_test['Title'].fillna(mode_title)
+
+
 # Model Initialization: Training a 
 # Logistic Regression classifier on the scaled training data
 scaler = StandardScaler()
@@ -63,7 +97,7 @@ print(f"Test dataset: {df_test.isnull().sum()}")
 
 
 # Train the classifier using the final processed feature matrix
-model = LogisticRegression()
+model = LogisticRegression(C = 0.05)
 model.fit(X_train_scaled,Y_train)
 
 #Predict result
@@ -98,11 +132,23 @@ print(propablistic)
 print(30*"---"+"\n")
 
 # performance metrics (Confusion Matrix and Classification Report)
-conf_matrix = confusion_matrix(Y_train, predictions)
-print(30*"---"+"\n")
-print("Confusion Matrix:")
-print(conf_matrix)
-print(30*"---"+"\n")
+confmat = confusion_matrix(y_true = Y_train, y_pred = predictions)
+print(30*"---")
+print("Output Confusion Matrix")
+print(confmat)
+print(30*"---")
+
+
+fix, ax  = plt.subplots(figsize  = (2.5, 2.5))
+ax.matshow(confmat, cmap = plt.cm.Blues, alpha = 0.3)
+for i in range(confmat.shape[0]):
+    for j in range(confmat.shape[1]):
+        ax.text(x = j, y = i, s = confmat[i, j], va = 'center', ha = 'center')
+
+plt.xlabel('prediction')
+plt.ylabel('True label')
+plt.show()
+
 
 # Model Evaluation: Generating predictions and computing 
 print(30*"---"+"\n")
